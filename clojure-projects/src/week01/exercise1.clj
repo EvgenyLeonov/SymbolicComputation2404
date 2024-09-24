8a3a711c494a3e6aa53f50ecf9c26816c3d33af0
Evgeny Leonov <leonov.e@praguecityuniversity.cz>
2024-09-24 11:55:11 +0200

    Week 01-1

--- clojure-projects/src/week01/exercise1.clj
+++ clojure-projects/src/week01/exercise1.clj
@@ -1,15 +1,15 @@
 (ns week01.exercise1)
 
-(println "1." (= __ true))
+(println "1." (= true true))
 
-(println "2." (= __ (+ 1 1)))
+(println "2." (= 2 (+ 1 1)))
 
-(println "3." (= (+ 3 4) 7 (+ 2 __)))
+(println "3." (= (+ 3 4) 7 (+ 2 5)))
 
-(println "4." (= __ (= 2 2/1)))
+(println "4." (= true (= 2 2/1)))
 
-(println "5." (= __ (= 2 2.0)))
+(println "5." (= false (= 2 2.0)))
 
-(println "6." (= __ (== 2.0 2)))
+(println "6." (= true (== 2.0 2)))
 
 
---
+++ clojure-projects/src/week01/lotr.clj
@@ -0,0 +1,7 @@
+(ns week01.lotr)
+
+(println "The total age of hobbits:" (+ 50 38 128))
+
+(println "Arwen and Aragorn:" (- 2777 87))
+
+
