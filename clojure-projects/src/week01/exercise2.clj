b575773248e5b1501a39c6e1bf9e4bc35a8197f1
Evgeny Leonov <leonov.e@praguecityuniversity.cz>
2024-09-26 08:38:43 +0200

    Week 01 exercise 2 added

---
+++ clojure-projects/src/week01/exercise2.clj
@@ -0,0 +1,15 @@
+(ns clojure-projects.week1.exercise2)
+
+(println "1." (= __ nil))
+
+(println "2." (= __ (not (= 1 nil))))
+
+(println "3." (= __ "hello"))
+
+(println "4." (= __ (str 'world)))
+
+(println "5." (= __ (= \c "c")))
+
+
+
+
