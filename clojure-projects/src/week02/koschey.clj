b1998c76098bdbfa7f097b611899dc3947e32a97
EvgenyLeonov <leonov.e@praguecityuniversity.cz>
2024-10-01 11:52:53 +0200

    Week 2-1

---
+++ clojure-projects/src/week02/koschey.clj
@@ -0,0 +1,19 @@
+(ns week02.koschey)
+
+; (fn [a b] (* a b))
+
+((fn [cst]
+   ((fn [jrb]
+      ((fn [dck]
+         ((fn [egg] (println egg)) dck)) jrb)
+      ) cst)
+   ) "Break the needle!")
+
+
+
+
+
+
+
+
+
