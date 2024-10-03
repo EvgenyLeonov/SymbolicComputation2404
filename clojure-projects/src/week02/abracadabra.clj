66494dbb40d141d52670f52cba1bd9a11a82b081
EvgenyLeonov <leonov.e@praguecityuniversity.cz>
2024-10-03 10:00:32 +0200

    exercise 1 prepared

---
+++ clojure-projects/src/week02/abracadabra.clj
@@ -0,0 +1,6 @@
+(ns week02.abracadabra)
+
+; please note: this is declaration!
+; (comp #(str % "abra") #(str % "cad") #(str "abra"))
+(println ((comp #(str % "abra") #(str % "cad") #(str "abra"))))
+
