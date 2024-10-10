95f012bd215369cd3133e4722453e3f6c0c2103a
EvgenyLeonov <leonov.e@praguecityuniversity.cz>
2024-10-10 14:04:31 +0200

    Update bird_watcher.clj

--- clojure-projects/src/week03/bird_watcher.clj
+++ clojure-projects/src/week03/bird_watcher.clj
@@ -23,9 +23,46 @@
   (count (filter #(>= % 5) birds))
   )
 
+; auxiliary functions (just for demo purposes)
+; options above are preferable and more compact
+
+(defn day-without-birds?-loop [birds]
+  (let [birds-count (count birds)]
+    (loop [index 0]
+      (if (< index birds-count)
+        (if (= 0 (nth birds index))
+          true
+          (recur (inc index))
+          )
+        false
+        )
+      )
+    )
+  )
+
+(defn busy-days-loop [birds]
+  (let [birds-count (count birds)]
+    (loop [index 0
+           current_busy_days_number 0]
+      (if (< index birds-count)
+        (if (>= (nth birds index) 5)
+          (recur (inc index) (inc current_busy_days_number))
+          (recur (inc index) current_busy_days_number)
+          )
+        current_busy_days_number
+        )
+      )
+    )
+  )
+
 (println "birds original =" birds)
 (println "today =" (today birds))
 (println "inc_bird =" (inc_bird birds))
 (println "day-without-birds? =" (day-without-birds? birds))
 (println "busy-days =" (busy-days birds))
+
+(println "--------")
+
+(println "day-without-birds?-loop =" (day-without-birds?-loop birds))
+(println "busy-days-loop =" (busy-days-loop birds))
 
