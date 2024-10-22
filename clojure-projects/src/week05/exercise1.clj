f4c2fd78b05119978f0e10dcbfc8787f54a454fe
EvgenyLeonov <leonov.e@praguecityuniversity.cz>
2024-10-22 11:48:14 +0200

    Update exercise1.clj

--- clojure-projects/src/week05/exercise1.clj
+++ clojure-projects/src/week05/exercise1.clj
@@ -3,38 +3,46 @@
 ; HINT: zero is even number
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
-  (loop [n   n
+  (loop [n n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 ; HINT please don't use "reverse" function
 (defn recursive-reverse [coll]
-  __)
+  (loop [index (dec (count coll))
+         result []]
+    (if (< index 0)
+      result
+      (recur (dec index) (conj result (get coll index)))
+      )
+    )
+  )
 
-(println "1." (= __ (is-even? 0)))
+; (println "recursive-reverse =" (recursive-reverse [1 2 3]))
 
-(println "2." (= __ (is-even? 1)))
+(println "recursive-reverse set simple =" (recursive-reverse (sorted-set 5 7 2 7)))
+(println "recursive-reverse set =" (apply list (recursive-reverse (vec (sorted-set 5 7 2 7)))))
 
-(println "3." (= __ (is-even-bigint? 100003N)))
+(println "1." (= true (is-even? 0)))
 
-(println "4." (= __ (recursive-reverse [1])))
+(println "2." (= false (is-even? 1)))
 
-(println "5." (= __ (recursive-reverse [2 3 4 5 6])))
+(println "3." (= false (is-even-bigint? 100003N)))
+
+(println "4." (= [1] (recursive-reverse [1])))
+
+(println "5." (= [6 5 4 3 2] (recursive-reverse [2 3 4 5 6])))
 
 ; NOTE create your own functions for tasks 6-8. Please don't use "reverse" function
 
-(println "6." (= (__ [1 2 3 4 5]) [5 4 3 2 1]))
+(println "6." (= (recursive-reverse [1 2 3 4 5]) [5 4 3 2 1]))
 
-(println "7." (= (__ (sorted-set 5 7 2 7)) '(7 5 2)))
+(println "7." (= (apply list (recursive-reverse (vec (sorted-set 5 7 2 7)))) '(7 5 2)))
 
-(println "8." (= (__ [[1 2][3 4][5 6]]) [[5 6][3 4][1 2]]))
-
-
-
-
+(println "8." (= (recursive-reverse [[1 2][3 4][5 6]]) [[5 6][3 4][1 2]]))
