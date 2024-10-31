3d652e672943b5b86b6c80a818549cd230588882
EvgenyLeonov <leonov.e@praguecityuniversity.cz>
2024-10-31 15:43:42 +0100

    Week 06 completed

--- clojure-projects/src/week06/dna.clj
+++ clojure-projects/src/week06/dna.clj
@@ -2,15 +2,27 @@
 
 (def dna "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGGC")
 
-(defn count_letter [letter text]
-  (count (filter #(= % letter) text))
-  )
+; trivial solution 1
+(println "frequencies =" (frequencies dna))
+(println "---------------")
 
+; trivial solution 2
+; each letter we take from string is char, not string
 (defn to_char [letter]
   (first (char-array letter))
   )
 
+(defn count_letter [letter text]
+  (count (filter #(= % letter) text))
+  )
+
 (println "A" (count_letter (to_char "A") dna))
+(println "C" (count_letter (to_char "C") dna))
+(println "T" (count_letter (to_char "T") dna))
+(println "G" (count_letter (to_char "G") dna))
+(println "---------------")
+
+; solution with grouping
 
 (defn count_letter_group [letter text]
   (-> #{letter}
@@ -21,6 +33,20 @@
   )
 
 (println "A" (count_letter_group (to_char "A") dna))
+(println "C" (count_letter_group (to_char "C") dna))
+(println "T" (count_letter_group (to_char "T") dna))
+(println "G" (count_letter_group (to_char "G") dna))
+(println "---------------")
 
-; #{\A \C \T \G}
-; TODO reduce, map
+; solution with high-order functions
+(defn count_letter_high_order
+  [dna]
+  (reduce (fn [result letter]
+            (->> dna
+                 (count_letter letter)
+                 (assoc result letter)
+                 ))
+          {}
+          #{\A \C \T \G}))
+
+(println "count_letter_high_order =" (count_letter_high_order dna))
