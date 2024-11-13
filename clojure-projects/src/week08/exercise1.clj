5065b80324882841e59f69415ada5b2a8600a3e8
EvgenyLeonov <leonov.e@praguecityuniversity.cz>
2024-11-13 09:35:48 +0100

    Week 08 in progress

--- clojure-projects/src/week08/exercise1.clj
+++ clojure-projects/src/week08/exercise1.clj
@@ -12,20 +12,20 @@
 (defn function-that-takes-a-coll [a b coll]
   (map :a coll))
 
-(println "1."  (= __
+(println "1."  (= {:a 1}
                   (-> {}
                       (assoc :a 1))))
 
-(println "2."   (= __
+(println "2."   (= "Hello world, and moon, and stars"
                    (-> "Hello world"
                        (str ", and moon")
                        (str ", and stars"))))
 
-(println "3."  (= __
+(println "3."  (= "String with a trailing space"
                   (-> "String with a trailing space "
                       clojure.string/trim)))
 
-(println "4."  (= __
+(println "4."  (= 6
                   (-> {}
                       (assoc :a 1)
                       (assoc :b 2)
@@ -34,23 +34,23 @@
                       (update-in [:c :e] inc)
                       (get-in [:c :e]))))
 
-(println "5."  (= __
+(println "5."  (= 1
                   (-> {}
                       (assoc :a 1)
                       (function-that-takes-a-map "hello" "there"))))
 
-(println "6."  (= __
+(println "6."  (= '(2 3 4)
                   (->> [1 2 3]
                        (map inc))))
 
-(println "7."  (= __
+(println "7."  (= 12
                   (->> a-list
                        (map inc)
                        (filter even?)
                        (into [])
                        (reduce +))))
 
-(println "8."  (= __
+(println "8."  (= [1 2 3]
                   (->> a-list-with-maps
                        (function-that-takes-a-coll "hello" "there")
                        (into []))))
