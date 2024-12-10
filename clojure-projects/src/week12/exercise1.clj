4d2b48b1c6b409909de4daae3f236258230f1cc1
EvgenyLeonov <leonov.e@praguecityuniversity.cz>
2024-12-10 08:50:46 +0100

    Create exercise1.clj

---
+++ clojure-projects/src/week12/exercise1.clj
@@ -0,0 +1,39 @@
+(ns week12.exercise1)
+
+(defmulti multimethod-without-args
+          (fn [keyword-arg] keyword-arg))
+
+(defmethod multimethod-without-args :first [_]
+  (str "Hello, World!"))
+
+(defmethod multimethod-without-args :second [_]
+  (str "Hello there"))
+
+(defmulti multimethod-with-args
+          (fn [opt-one opt-two] opt-one))
+
+(defmethod multimethod-with-args :path-one [_ opts]
+  (:first-opt opts))
+
+(defmethod multimethod-with-args :path-two [_ opts]
+  (let [numbers (:second-opt opts)]
+    (->> numbers
+         (map inc)
+         (reduce +))))
+
+(defmethod multimethod-with-args :path-three [_])
+
+(println "1."  (= __
+                  (multimethod-without-args :first)))
+
+(println "2."  (= __
+                  (multimethod-without-args :second)))
+
+(println "3."  (= __
+                  (multimethod-with-args :path-one {:first-opt 1
+                                                    :second-opt 2})))
+
+(println "4."  (= __
+                  (multimethod-with-args :path-two {:first-opt 1
+                                                    :second-opt [0 1 2]})))
+
