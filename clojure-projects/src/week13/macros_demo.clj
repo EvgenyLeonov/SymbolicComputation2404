eba0d159cd31139f05ab034b42cfbb25ac353b1f
EvgenyLeonov <leonov.e@praguecityuniversity.cz>
2025-01-07 09:39:35 +0100

    week13 WIP

---
+++ clojure-projects/src/week13/macros_demo.clj
@@ -0,0 +1,24 @@
+(ns week13.macros-demo)
+
+(defmacro my_when
+  [cond & body]
+  `(if ~cond
+     (do ~@body)
+     )
+  )
+
+(my_when (> 6 5)
+         (println "print 1")
+         (println "print 2")
+         (println "print 3")
+         (println "print 4")
+         (inc 1)
+         )
+
+(macroexpand-1 '(my_when (> 6 5)
+                        (println "print 1")
+                        (println "print 2")
+                        (println "print 3")
+                        (println "print 4")
+                        (inc 1)
+                        ))
---
+++ clojure-projects/src/week13/symbolic_computations.clj
@@ -0,0 +1,27 @@
+(ns week13.symbolic-computations)
+
+; https://clojure.org/guides/weird_character
+
+; run it in REPL
+; quote
+'(1 3 4)
+
+; syntax quote
+`(1 2 3)
+
+; unquote
+; ~ calculates the form
+`(1 2 3 ~(+ 4 5))
+
+(def l [4 5 6])
+
+`(1 2 3 ~l)
+
+; Unquote splicing
+`(1 2 3 ~@l)
+
+(def my_funcs [inc dec last])
+
+`(1 2 3 ~@my_funcs)
+
+
